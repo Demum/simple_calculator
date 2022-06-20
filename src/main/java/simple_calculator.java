2d0f45421188c8a40a3bdc2bd44ce968c942b8da
Yauhen Shviatsou <demum1930@gmail.com>
2022-06-21 00:15:17 +0300

    second version on english

--- src/main/java/simple_calculator.java
+++ src/main/java/simple_calculator.java
@@ -4,16 +4,16 @@ public class simple_calculator {
     public static void main(String[] args) {
          Scanner scanner = new Scanner(System.in);
          int a,b;
-        System.out.println("Сложение: Введите два числа (пример, 12 8): ");
+        System.out.println("Sum of 2 digits (example, 12 8): ");
          a=scanner.nextInt();
          b=scanner.nextInt();
 
-        System.out.println("Cумма "+a+" + "+b+" = " + sum(a,b));
+        System.out.println("Sum "+a+" + "+b+" = " + sum(a,b));
 
-        System.out.println("Умножение: Введите два числа (пример, 4 5): ");
+        System.out.println("Multiplying: enter two digits (example, 4 5): ");
         a=scanner.nextInt();
         b=scanner.nextInt();
-        System.out.println("Произведение "+a+" * "+b +" = "+ multyply(a,b));
+        System.out.println("Multiplying "+a+" * "+b +" = "+ multyply(a,b));
     }
 
        static   int sum ( int a, int b){
